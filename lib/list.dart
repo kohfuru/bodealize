9fa4633c977a5d5eabb04e860b75b0629a14dc52
Kohei Furutani <12koh07@gmail.com>
2023-11-09 17:57:47 +0900

    lib/list.dartの追加

---
+++ lib/list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ListWidget extends StatefulWidget {
+  const ListWidget({super.key});
+
+  @override
+  State<ListWidget> createState() => _ListWidgetState();
+}
+
+class _ListWidgetState extends State<ListWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+}
