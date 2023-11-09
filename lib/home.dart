8b756de4d74917102d94aeab1e3d5004aa13192e
Kohei Furutani <12koh07@gmail.com>
2023-11-09 17:46:04 +0900

    lib/home.dart内のTableCalendarとTextFormの箇所を適切に修正

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:bodealize/calender.dart';
+import 'package:bodealize/input_form.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
-
   final String title;
 
   @override
@@ -11,13 +12,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // ignore: non_constant_identifier_names
   String user_name = 'ユーザー名';
+
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  // ignore: non_constant_identifier_names
   final event_data = {
     DateTime.utc(2023, 6, 26): ['firstEvent']
   };
-  List<String> _selectedEvents = [];
 
   @override
   Widget build(BuildContext context) {
@@ -28,55 +31,18 @@ class _HomePageState extends State<HomePage> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          TableCalendar(
-            firstDay: DateTime.utc(2010, 1, 1),
-            lastDay: DateTime.utc(2050, 12, 31),
+          CalendarWidget(
             focusedDay: _focusedDay,
-            headerStyle: const HeaderStyle(
-              formatButtonVisible: false,
-            ),
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
+            selectedDay: _selectedDay,
+            eventData: event_data,
             onDaySelected: (selectedDay, focusedDay) {
               setState(() {
                 _selectedDay = selectedDay;
-                _focusedDay = focusedDay; // update `_focusedDay` here as well
+                _focusedDay = focusedDay;
               });
             },
-            eventLoader: (date) {
-              return event_data[date] ?? [];
-            },
           ),
-          Row(
-            children: [
-              Container(
-                width: 325,
-                margin: const EdgeInsets.only(left: 10),
-                child: const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        )
-                    ),
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 4,
-                      horizontal: 4,
-                    ),
-                  ),
-                ),
-              ),
-              Container(
-                // width: 100,
-                margin: const EdgeInsets.only(left: 10),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('button'),
-                ),
-              )
-            ]
-          ),
+          const InputForm(),
         ],
       )
     );
