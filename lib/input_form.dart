2debd819186e91d55019dd123b65a8c1b1515d4d
Kohei Furutani <12koh07@gmail.com>
2023-11-09 17:57:02 +0900

    インデントの修正

--- lib/input_form.dart
+++ lib/input_form.dart
@@ -12,33 +12,33 @@ class _InputFormState extends State<InputForm> {
   @override
   Widget build(BuildContext context) {
     return Row(
-        children: [
-          Container(
-            width: 325,
-            margin: const EdgeInsets.only(left: 10),
-            child: const TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.black,
-                    )
-                ),
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: 4,
-                  horizontal: 4,
-                ),
+      children: [
+        Container(
+          width: 325,
+          margin: const EdgeInsets.only(left: 10),
+          child: const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                )
+              ),
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 4,
+                horizontal: 4,
               ),
             ),
           ),
-          Container(
-            // width: 100,
-            margin: const EdgeInsets.only(left: 10),
-            child: ElevatedButton(
-              onPressed: () {},
-              child: const Text('button'),
-            ),
-          )
-        ]
+        ),
+        Container(
+          // width: 100,
+          margin: const EdgeInsets.only(left: 10),
+          child: ElevatedButton(
+            onPressed: () {},
+            child: const Text('button'),
+          ),
+        )
+      ]
     );
   }
 }
