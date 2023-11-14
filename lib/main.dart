841de08f9d802b0c2f0d3f2b35e972ad599ed92f
Kohei Furutani <12koh07@gmail.com>
2023-11-14 18:23:29 +0900

    Firebaseの初期化

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 // import 'package:bodealize/footer.dart';
 import 'package:bodealize/home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
