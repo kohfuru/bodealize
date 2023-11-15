e92c2f79f0c421598406e59815561a826eeec3d2
Kohei Furutani <12koh07@gmail.com>
2023-11-15 15:11:16 +0900

    lib/home.dartの削除

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 // import 'package:bodealize/footer.dart';
-// import 'package:bodealize/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
