097cfb04152028370fe4f870ab94f855f658e10a
Kohei Furutani <12koh07@gmail.com>
2023-11-20 17:12:48 +0900

    lib/calendar_page/modal_textfield.dartを追加

---
+++ lib/calendar_page/modal_textfield.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ModalTextField extends StatefulWidget {
+  const ModalTextField({super.key});
+
+  @override
+  State<ModalTextField> createState() => _ModalTextFieldState();
+  
+}
+
+class _ModalTextFieldState extends State<ModalTextField> {
+  @override
+  Widget build(BuildContext context) {
+    return 
+  }
+}
