90ce34acf5de61ebb4c509578bffca69014416f2
Kohei Furutani <12koh07@gmail.com>
2023-11-22 16:12:16 +0900

    CupertinoAlertDialogを設置

--- lib/category/category_navigationbar.dart
+++ lib/category/category_navigationbar.dart
@@ -21,68 +21,71 @@ class _CategoryBottomState extends State<CategoryBottom> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 85,
-      decoration: BoxDecoration(
-        color: Colors.blue.shade50,
-        border: const Border(
-          top: BorderSide(
-            color: Colors.blueGrey,
-            width: 1
-          )
-        )
-      ),
-      child: GestureDetector(
-        onTapDown: (_) {
-          setState(() {
-            isPressed = true;
-          });
-        },
-        onTapUp: (_) {
-          setState(() {
-            isPressed = false;
-          });
-          showCupertinoModalPopup(
-            context: context,
-            builder: (BuildContext context) {
-              return CupertinoActionSheet(
-                title: const Text('カテゴリを追加'),
-                actions: [
-                  CupertinoTextField(
-                    controller: _textController,
-                    placeholder: 'カテゴリを追加',
-                    keyboardType: TextInputType.text,
-                  ),
-                ],
-                cancelButton: CupertinoActionSheetAction(
-                  child: const Text('キャンセル'),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              );
-            },
-          );
-        },
-        onTapCancel: () {
-          setState(() {
-            isPressed = false;
-          });
-        },
-        child: Align(
-          alignment: Alignment.topRight,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'カテゴリ追加',
-              style: TextStyle(
-                color: isPressed ? Colors.orange.withOpacity(0.5) : Colors.orange,
-                fontSize: 15
-              ),
+    return Material(
+        child: Container(
+            height: 85,
+            decoration: BoxDecoration(
+                color: Colors.blue.shade50,
+                border: const Border(
+                    top: BorderSide(
+                        color: Colors.blueGrey,
+                        width: 1
+                    )
+                )
             ),
-          ),
+            child: GestureDetector(
+                onTapDown: (_) {
+                  setState(() {
+                    isPressed = true;
+                  });
+                },
+                onTapUp: (_) {
+                  setState(() {
+                    isPressed = false;
+                  });
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return CupertinoAlertDialog(
+                        title: const Text('カテゴリを追加'),
+                        content: Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: CupertinoTextField(controller: _textController,),
+                        ),
+                        actions: [
+                          CupertinoDialogAction(
+                            child: Text('キャンセル'),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                          CupertinoDialogAction(
+                            child: Text('OK'),
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                onTapCancel: () {
+                  setState(() {
+                    isPressed = false;
+                  });
+                },
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'カテゴリ追加',
+                      style: TextStyle(
+                          color: isPressed ? Colors.orange.withOpacity(0.5) : Colors.orange,
+                          fontSize: 15
+                      ),
+                    ),
+                  ),
+                )
+            )
         )
-      )
     );
   }
 }
