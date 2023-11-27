c7ff3ecfafda595f92ff61b4773832204d3c59a7
Kohei Furutani <12koh07@gmail.com>
2023-11-27 17:47:00 +0900

    ログインボタン、新規登録ページへの遷移ボタンを設置

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,5 +1,7 @@
 import 'package:bodealize/component/appbar.dart';
 import 'package:bodealize/component/clear_button.dart';
+import 'package:bodealize/login/signup.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -23,20 +25,18 @@ class _LoginState extends State<Login> {
 
     Padding textField(double paddingTop, TextEditingController controller) {
       return Padding(
-          padding: EdgeInsets.only(top: paddingTop),
-          child: Center(
-            child: SizedBox(
-              width: textFieldWidth,
-              height: textFieldHeight,
-              child: TextFormField(
-                controller: controller,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    suffixIcon: ClearButton(controller: controller)
-                ),
-              ),
+        padding: EdgeInsets.only(top: paddingTop),
+        child: SizedBox(
+          width: textFieldWidth,
+          height: textFieldHeight,
+          child: TextFormField(
+            controller: controller,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              suffixIcon: ClearButton(controller: controller)
             ),
-          )
+          ),
+        ),
       );
     }
 
@@ -45,12 +45,62 @@ class _LoginState extends State<Login> {
         title: 'ログイン画面',
         actions: [],
       ),
-      body: Column(
-        children: [
-          textField(100, _emailController),
-          textField(50, _passController)
-        ],
-      ),
+      body: Center(
+        child: Column(
+          children: [
+            textField(100, _emailController),
+            textField(50, _passController),
+            Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: SizedBox(
+                width: screenWidth * 0.6,
+                height: textFieldHeight,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    fixedSize: const Size(120, 60),
+                    backgroundColor: Colors.orange
+                  ),
+                  child: const Text(
+                    'ログイン',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+                    )
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10),
+              child: RichText(
+                text: TextSpan(
+                  style: const TextStyle(color: Colors.black),
+                  children: [
+                    const TextSpan(text: '新規登録は '),
+                    TextSpan(
+                      text: 'こちら',
+                      style: const TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                      recognizer: TapGestureRecognizer()
+                      .. onTap = () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const SignUp(),
+                          ),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
