462cde1e5debe9434e408a8892b4d839eced135c
Kohei Furutani <12koh07@gmail.com>
2023-11-28 16:06:38 +0900

    ログイン画面のテキストフィールドをコンポーネント化

---
+++ lib/login/login_textfield.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../component/clear_button.dart';
+
+class LoginTextField extends StatefulWidget {
+  final double paddingTop;
+  final TextEditingController controller;
+  final String hintText;
+
+  const LoginTextField({
+    required this.paddingTop,
+    required this.controller,
+    required this.hintText,
+    super.key
+  });
+
+  @override
+  State<LoginTextField> createState() => _LoginTextFieldState();
+}
+
+class _LoginTextFieldState extends State<LoginTextField> {
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    double textFieldWidth = screenWidth * 0.8;
+    double textFieldHeight = screenHeight * 0.06;
+
+    return Padding(
+      padding: EdgeInsets.only(top: widget.paddingTop),
+      child: SizedBox(
+        width: textFieldWidth,
+        height: textFieldHeight,
+        child: TextFormField(
+          controller: widget.controller,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            suffixIcon: ClearButton(controller: widget.controller),
+            hintText: widget.hintText,
+            contentPadding: const EdgeInsets.only(top: 30, left: 10),
+          ),
+        ),
+      ),
+    );
+  }
+}
