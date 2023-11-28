fdd3516a92d6713af155b9188db3eaafd8df7ed3
Kohei Furutani <12koh07@gmail.com>
2023-11-28 16:09:59 +0900

    LoginTextFieldウィジェットとLoginButtonウィジェットを使用するように変更 ユーザー名入力欄を追加

--- lib/login/signup.dart
+++ lib/login/signup.dart
@@ -1,5 +1,6 @@
 import 'package:bodealize/component/appbar.dart';
-import 'package:bodealize/login/body_com.dart';
+import 'package:bodealize/login/login_button.dart';
+import 'package:bodealize/login/login_textfield.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -10,19 +11,42 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _userNameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: AppBarWidget(
+    return Scaffold(
+      appBar: const AppBarWidget(
         title: '新規登録',
         actions: [],
       ),
       body: Center(
-        child: LoginBody(
-          onPressed: null,
-          text: '新規登録'
-        ),
+        child: Column(
+          children: [
+            LoginTextField(
+              paddingTop: 100,
+              controller: _emailController,
+              hintText: 'メールアドレス'
+            ),
+            LoginTextField(
+              paddingTop: 35,
+              controller: _passController,
+              hintText: 'パスワード'
+            ),
+            LoginTextField(
+              paddingTop: 35,
+              controller: _userNameController,
+              hintText: 'ユーザー名'
+            ),
+            const LoginButton(
+              paddingTop: 35,
+              onPressed: null,
+              text: '新規登録',
+            )
+          ],
+        )
       )
     );
   }
