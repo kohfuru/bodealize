9f8f35496ed4e058463e2c7c05e713e05a1c1d1d
Kohei Furutani <12koh07@gmail.com>
2023-12-04 22:08:01 +0900

    ログイン画面の新規登録画面げの遷移をクラスで管理

---
+++ lib/login/to_signup.dart
@@ -0,0 +1,37 @@
+import 'package:bodealize/login/signup_ui.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class ToSignUp extends StatelessWidget {
+  const ToSignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: RichText(
+        text: TextSpan(
+          style: const TextStyle(color: Colors.black),
+          children: [
+            const TextSpan(text: '新規登録は '),
+            TextSpan(
+              text: 'こちら',
+              style: const TextStyle(
+                color: Colors.blue,
+                decoration: TextDecoration.underline,
+              ),
+              recognizer: TapGestureRecognizer()
+                .. onTap = () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const SignUp(),
+                    ),
+                  );
+                },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
