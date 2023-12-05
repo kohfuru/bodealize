51b8f3890f2582343134dd4eb2ed4783a6307fe4
Kohei Furutani <12koh07@gmail.com>
2023-12-05 17:02:04 +0900

    dialogactionのonPressedの値をonPressedに変更

--- lib/component/error_dialog.dart
+++ lib/component/error_dialog.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void showErrorDialog(context, title, text) {
+// () => Navigator.pop(context)
+void showErrorDialog(context, title, text, onPressed) {
   showDialog(
     context: context,
+    barrierDismissible: true,
     builder: (context) {
       return CupertinoAlertDialog(
         title: Text(title),
         content: Text(text),
         actions: [
           CupertinoDialogAction(
+            onPressed: onPressed,
             child: const Text('OK'),
-            onPressed: () => Navigator.pop(context),
           ),
         ],
       );
