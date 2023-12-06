7a668ddd88986a19e71c1a0ad08f4f753de68f68
Kohei Furutani <12koh07@gmail.com>
2023-12-06 18:42:47 +0900

    ボタンの構造を統一

--- lib/component/my_button.dart
+++ lib/component/my_button.dart
@@ -4,11 +4,13 @@ class MyButton extends StatefulWidget {
   final VoidCallback? onPressed;
   final String text;
   final double size;
+  final double paddingTop;
 
   const MyButton({
     required this.onPressed,
     required this.text,
     required this.size,
+    required this.paddingTop,
     super.key
   });
 
@@ -17,22 +19,34 @@ class MyButton extends StatefulWidget {
 }
 
 class _MyButtonState extends State<MyButton> {
+
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: widget.onPressed,
-      style: TextButton.styleFrom(
-        fixedSize: const Size(120, 60),
-        backgroundColor: Colors.orange
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    double textFieldHeight = screenHeight * 0.06;
+
+    return Padding(
+      padding: EdgeInsets.only(top: widget.paddingTop),
+      child:  SizedBox(
+        width: screenWidth * 0.6,
+        height: textFieldHeight,
+        child: TextButton(
+          onPressed: widget.onPressed,
+          style: TextButton.styleFrom(
+            backgroundColor: Colors.orange
+          ),
+          child: Text(
+            widget.text,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: widget.size,
+            ),
+          ) ,
+        )
       ),
-      child: Text(
-        widget.text,
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: widget.size,
-        ),
-      ) ,
     );
   }
 }
