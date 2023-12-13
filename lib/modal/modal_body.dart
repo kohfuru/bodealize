89b26d3676f38b9c299a78394c9a438a7090cb45
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2023-12-13 17:46:38 +0900

    全体をMaterialウィジェットで囲うように変更

--- lib/modal/modal_body.dart
+++ lib/modal/modal_body.dart
@@ -6,7 +6,6 @@ import 'package:bodealize/modal/select_categoryfield.dart';
 
 import '../component/my_button.dart';
 
-
 class ModalBody extends StatefulWidget {
   const ModalBody({super.key});
 
@@ -23,45 +22,47 @@ class _ModalBodyState extends State<ModalBody> {
   @override
   Widget build(BuildContext context) {
     this.context =context;
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ModalTextField(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const MenuList(),
-                ),
-              );
-            },
-            icon: Icons.list,
-            controller: _menuNameController,
-            hintText: 'メニュー名',
-            autofocus: true,
-            height: 50,
-            padding: 0,
-          ),
-          const Divider(color: Colors.grey),
-          const SelectCategory(),
-          const Divider(color: Colors.grey),
-          ModalTextField(
-            onPressed: null,
-            icon: Icons.menu_book,
-            controller: _memoController,
-            hintText: 'メモを入力',
-            autofocus: false,
-            height: 150,
-            padding: 50,
-          ),
-          const Divider(color: Colors.grey),
-          MyButton(
-            onPressed: () {},
-            text: '保存',
-            size: 15,
-            paddingTop: 20,
+    return Material(
+      child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ModalTextField(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const MenuList(),
+                    ),
+                  );
+                },
+                icon: Icons.list,
+                controller: _menuNameController,
+                hintText: 'メニュー名',
+                autofocus: true,
+                height: 50,
+                padding: 0,
+              ),
+              const Divider(color: Colors.grey),
+              SelectCategory(),
+              const Divider(color: Colors.grey),
+              ModalTextField(
+                onPressed: null,
+                icon: Icons.menu_book,
+                controller: _memoController,
+                hintText: 'メモを入力',
+                autofocus: false,
+                height: 150,
+                padding: 50,
+              ),
+              const Divider(color: Colors.grey),
+              MyButton(
+                onPressed: () {},
+                text: '保存',
+                size: 15,
+                paddingTop: 20,
+              )
+            ],
           )
-        ],
-      )
+      ),
     );
   }
 }
