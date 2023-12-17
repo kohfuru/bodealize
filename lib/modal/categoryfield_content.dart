0819b82da9a8a8fd79109cc12c1a969fc28cbba4
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2023-12-17 15:59:03 +0900

    左右にpaddingを指定

--- lib/modal/categoryfield_content.dart
+++ lib/modal/categoryfield_content.dart
@@ -19,38 +19,41 @@ class CategoryFieldContent extends StatefulWidget {
 class _CategoryFieldContentState extends State<CategoryFieldContent> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: InkWell(
-        onTap: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const CategoryPage(),
-            ),
-          );
-        },
-        highlightColor: Colors.white,
-        splashColor: Colors.transparent,
-        child: Row(
-          children: [
-            Icon(
-              Icons.lens,
-              color: Color(widget.categoryColor),
-            ),
-            const Padding(padding: EdgeInsets.only(left: 12)),
-            Text(
-              widget.categoryName,
-              style: const TextStyle(
-                  color: Colors.black,
-                  fontSize: 20
+    return Padding(
+      padding: const EdgeInsets.only(left: 11, right: 11),
+      child: SizedBox(
+        height: 50,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => const CategoryPage(),
               ),
-            ),
-            const Spacer(),
-            Icon(
-              Icons.arrow_forward_ios,
-              color: Colors.grey.shade600,
-            ),
-          ],
+            );
+          },
+          highlightColor: Colors.white,
+          splashColor: Colors.transparent,
+          child: Row(
+            children: [
+              Icon(
+                Icons.lens,
+                color: Color(widget.categoryColor),
+              ),
+              const Padding(padding: EdgeInsets.only(left: 12)),
+              Text(
+                widget.categoryName,
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 20
+                ),
+              ),
+              const Spacer(),
+              Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.grey.shade600,
+              ),
+            ],
+          ),
         ),
       ),
     );
