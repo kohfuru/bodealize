771028101cf3e57c1fbd6b738c3392a5e994ede8
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2023-12-21 15:12:44 +0900

    日付選択がされていない時にデザインの崩れがあったため修正

--- lib/modal/datefield_content.dart
+++ lib/modal/datefield_content.dart
@@ -15,21 +15,21 @@ class _DateFieldContentState extends State<DateFieldContent> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 10),
-      child: StreamBuilder<QuerySnapshot>(
-        stream: firestoreReference.selectedDay.snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return const Text('Something went wrong');
-          }
+    return StreamBuilder<QuerySnapshot>(
+      stream: firestoreReference.selectedDay.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
 
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text("Loading");
-          }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
 
-          if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
-            return SizedBox(
+        if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+          return Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: SizedBox(
               width: 300,
               child: ListView(
                 children: snapshot.data!.docs.map((DocumentSnapshot document) {
@@ -42,17 +42,17 @@ class _DateFieldContentState extends State<DateFieldContent> {
                   );
                 }).toList(),
               )
-            );
-          }
-
-          return const Text(
-            '日付を選択してください',
-            style: TextStyle(
-              fontSize: 20
             ),
           );
         }
-      ),
+
+        return const Text(
+          '日付を選択してください',
+          style: TextStyle(
+            fontSize: 20
+          ),
+        );
+      }
     );
   }
 }
