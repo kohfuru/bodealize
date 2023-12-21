d0bb9b343b2dae2117c6b18b001de65e3680a13c
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2023-12-21 15:07:03 +0900

    日付を選択する処理部分をlib/modal/date_select.dartファイルに記述

--- lib/modal/date_select.dart
+++ lib/modal/date_select.dart
@@ -1,23 +1,20 @@
-import 'package:bodealize/firestore_reference.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
-class DateSelect extends StatefulWidget {
-  const DateSelect({super.key});
+import '../firestore_reference.dart';
 
-  @override
-  State<DateSelect> createState() => _DateSelectState();
-}
-
-class _DateSelectState extends State<DateSelect> {
+class SelectDate {
   FirestoreReference firestoreReference = FirestoreReference();
-  final iconColor = Colors.black;
   DateTime _date = DateTime.now();
-  late String formattedDateTime = '日付を選択してください';
 
-  Future<Null> _selectDate(context) async {
+  Future<bool> checkCollectionExists() async {
+    final QuerySnapshot snapshot = await firestoreReference.selectedDay.get();
+    return snapshot.docs.isEmpty;
+  }
+
+  Future<Null> selectDate(context) async {
     await initializeDateFormatting('ja_JP');
 
     final DateTime? picked = await showDatePicker(
@@ -28,60 +25,14 @@ class _DateSelectState extends State<DateSelect> {
     );
 
     if (picked != null) {
-      // setState(() {
-      //   _date = picked;
-      //   formattedDateTime = DateFormat('yyyy年MM月dd日（EEE）', 'ja_JP').format(_date);
-      // });
-
-      Future<bool> checkCollectionExists() async {
-        final QuerySnapshot snapshot = await firestoreReference.selectedDay.get();
-        return snapshot.docs.isEmpty;
-      }
-
-      bool collectionExists = await checkCollectionExists();
       _date = picked;
+      String formattedDateTime = DateFormat('yyyy年MM月dd日（EEE）', 'ja_JP').format(_date);
 
-      if (collectionExists) {
-        firestoreReference.lastSelectedDay.set({'date': _date});
+      if (await checkCollectionExists()) {
+        firestoreReference.lastSelectedDay.set({'date': formattedDateTime});
       } else {
-        firestoreReference.lastSelectedDay.update({'date': _date});
+        firestoreReference.lastSelectedDay.update({'date': formattedDateTime});
       }
     }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: InkWell(
-        onTap: () => _selectDate(context),
-        highlightColor: Colors.white,
-        splashColor: Colors.transparent,
-        child: Row(
-          children: [
-            IconButton(
-              onPressed: null,
-              icon: Icon(
-                Icons.calendar_today,
-                color: iconColor,
-              )
-            ),
-            const Padding(padding: EdgeInsets.only(left: 12)),
-            Text(
-              formattedDateTime,
-              style: const TextStyle(
-                fontSize: 20
-              ),
-            ),
-            const Spacer(),
-            Icon(
-              Icons.arrow_drop_down,
-              color: iconColor,
-              size: 50,
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
--- lib/modal/modal_datefield.dart
+++ lib/modal/modal_datefield.dart
@@ -1,9 +1,7 @@
-import 'package:bodealize/firestore_reference.dart';
-import 'package:bodealize/modal/datefield_content.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
+
+import 'datefield_content.dart';
+import 'date_select.dart';
 
 class DateSelect extends StatefulWidget {
   const DateSelect({super.key});
@@ -14,43 +12,14 @@ class DateSelect extends StatefulWidget {
 
 class _DateSelectState extends State<DateSelect> {
   final iconColor = Colors.black;
-  FirestoreReference firestoreReference = FirestoreReference();
-  DateTime _date = DateTime.now();
-  late String formattedDateTime;
-
-  Future<bool> checkCollectionExists() async {
-    final QuerySnapshot snapshot = await firestoreReference.selectedDay.get();
-    return snapshot.docs.isEmpty;
-  }
-
-  Future<Null> selectDate(context) async {
-    await initializeDateFormatting('ja_JP');
-
-    final DateTime? picked = await showDatePicker(
-        context: context,
-        firstDate: DateTime(2000),
-        lastDate: DateTime(2050),
-        initialDate: _date
-    );
-
-    if (picked != null) {
-      _date = picked;
-      String formattedDateTime = DateFormat('yyyy年MM月dd日（EEE）', 'ja_JP').format(_date);
-
-      if (await checkCollectionExists()) {
-        firestoreReference.lastSelectedDay.set({'date': formattedDateTime});
-      } else {
-        firestoreReference.lastSelectedDay.update({'date': formattedDateTime});
-      }
-    }
-  }
+  SelectDate selectDate = SelectDate();
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
       child: InkWell(
-        onTap: () => selectDate(context),
+        onTap: () => selectDate.selectDate(context),
         highlightColor: Colors.white,
         splashColor: Colors.transparent,
         child: Row(
