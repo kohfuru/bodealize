7ba1d504507d86fb325fee8ff750e9cde141d380
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2023-12-22 18:03:38 +0900

    ButtonBarContentの別ファイルへの切り分けによる変更

--- lib/modal/save_menu.dart
+++ lib/modal/save_menu.dart
@@ -2,38 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:bodealize/firestore_reference.dart';
+import 'dialog_buttonbar_content.dart';
 
 class SaveMenu {
   FirestoreReference firestoreReference = FirestoreReference();
   late String date;
 
-  Future<bool> checkDocExists() async {
-    final QuerySnapshot snapshot = await firestoreReference.menus.get();
-    return snapshot.docs.isEmpty;
-  }
-
-  SizedBox buttonBarContent(onPressed, backColor, text) {
-    return SizedBox(
-      width: 100,
-      child: TextButton(
-        onPressed: onPressed,
-        style: TextButton.styleFrom(
-            backgroundColor: backColor
-        ),
-        child: Text(
-          text,
-          style: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold
-          ),
-        ),
-      ),
-    );
-  }
-
   Future saveMenu(TextEditingController menuName, TextEditingController memo, BuildContext context) async {
     if (menuName.text.isNotEmpty) {
-
       firestoreReference.lastSelectedDay.get().then((DocumentSnapshot doc) async {
         final data = doc.data() as Map<String, dynamic>;
         date = data['date'];
@@ -46,45 +22,43 @@ class SaveMenu {
         };
 
         menuCol.add(menuData);
-        print('test1');
 
         return showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                backgroundColor: Colors.white,
-                title: const Text('メニューを保存しました'),
-                actions: [
-                  ButtonBar(
-                    alignment: MainAxisAlignment.center,
-                    children: [
-                      buttonBarContent(
-                              () {
-                            Navigator.pop(context);
-                            Navigator.pop(context);
-                          },
-                          Colors.black45,
-                          '完了'
-                      ),
-                      buttonBarContent(
-                              () {
-                            Navigator.pop(context);
-                            menuName.clear();
-                            memo.clear();
-                          },
-                          Colors.orange,
-                          '続けて入力'
-                      ),
-                    ],
-                  )
-                ],
-              );
-            }
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              backgroundColor: Colors.white,
+              title: const Text('メニューを保存しました'),
+              actions: [
+                ButtonBar(
+                  alignment: MainAxisAlignment.center,
+                  children: [
+                    buttonBarContent(
+                      () {
+                        Navigator.pop(context);
+                        Navigator.pop(context);
+                      },
+                      Colors.black45,
+                      '完了'
+                    ),
+                    buttonBarContent(
+                      () {
+                        Navigator.pop(context);
+                        menuName.clear();
+                        memo.clear();
+                      },
+                      Colors.orange,
+                      '続けて入力'
+                    ),
+                  ],
+                )
+              ],
+            );
+          }
         );
       });
     } else {
       return;
     }
-
   }
 }
