1017ac41eb9237e0c4c31c8fb072c746630b1d03
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2023-12-31 16:30:37 +0900

    DatePickerで日付を選択してメニューを複製する処理を実装

---
+++ lib/calendar_page/copy_menu.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:bodealize/firestore_reference.dart';
+
+class CopyMenu {
+  FirestoreReference firestoreReference = FirestoreReference();
+  DateTime _date = DateTime.now();
+
+  Future copyMenu(BuildContext context, String menuName, String memo) async {
+    final DateTime? picked = await showDatePicker(
+        context: context,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2050),
+        initialDate: _date
+    );
+
+    if (picked != null) {
+      _date = picked;
+
+      var menuData = {
+        'menuName': menuName,
+        'memo': memo,
+        'date': _date,
+        'flag': false
+      };
+
+      firestoreReference.menus.add(menuData);
+    }
+  }
+}
