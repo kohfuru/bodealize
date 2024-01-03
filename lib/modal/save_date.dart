07c65d129315ce05ddd292089bd88f6931ef5451
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2024-01-03 17:10:31 +0900

    lib/modal/select_date.dartをlib/modal/save_date.dartに変更 日付保存時の不要な処理を削除

---
+++ lib/modal/save_date.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import '../firestore_reference.dart';
+
+Future<Null> saveDate(BuildContext context, DateTime focusedDay) async {
+  FirestoreReference firestoreReference = FirestoreReference();
+  await initializeDateFormatting('ja_JP');
+  String formattedDateTime = DateFormat('yyyy年MM月dd日（EEE）', 'ja_JP').format(focusedDay);
+
+  firestoreReference.lastSelectedDay.set({
+    'dateString': formattedDateTime,
+    'date': focusedDay
+  });
+}
