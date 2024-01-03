9b7b1967aba3cc4cf96f88c2bfbe3d63ab2f916f
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2024-01-03 19:03:15 +0900

    MenuWidgetを呼び出す際の引数を削除’

--- lib/calendar_page/calendar_page.dart
+++ lib/calendar_page/calendar_page.dart
@@ -21,7 +21,7 @@ class _CalendarPageState extends State<CalendarPage> {
           color: Colors.white,
           child: const CalendarWidget(),
         ),
-        Expanded(child: MenuWidget(focusedDay: _focusedDay)),
+        Expanded(child: MenuWidget()),
       ],
     );
   }
