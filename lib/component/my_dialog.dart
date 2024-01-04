8dba98da73dec9f60d0b5e89ef1e830fd6ad6f16
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2024-01-04 15:38:54 +0900

    iOSとAndroidでダイアログのデザインが変わるよう修正

--- lib/component/my_dialog.dart
+++ lib/component/my_dialog.dart
@@ -1,20 +1,46 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void myDialog(context, String title, String text, VoidCallback? onPressed) {
-  showDialog(
-    context: context,
-    barrierDismissible: true,
-    builder: (context) {
-      return AlertDialog(
-        title: Text(title),
-        content: Text(text),
-        actions: [
-          FloatingActionButton(
-            onPressed: onPressed,
-            child: const Text('OK'),
-          ),
-        ],
-      );
-    }
-  );
+Future myDialog(context, String title, String text, VoidCallback? onPressed) async {
+  if (Platform.isAndroid) {
+    await showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(text),
+          actions: [
+            FloatingActionButton(
+              onPressed: onPressed,
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      }
+    );
+  } else if (Platform.isIOS) {
+    await showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: Text(title),
+          content: Text(text),
+          actions: [
+            TextButton(
+              onPressed: onPressed,
+              child: const Text(
+                'OK',
+                style: TextStyle(
+                  fontSize: 18
+                ),
+              ),
+            ),
+          ],
+        );
+      }
+    );
+  }
 }
