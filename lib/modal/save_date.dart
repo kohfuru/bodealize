bbea6bc3236da137c994a7dccf65e7fb11a37cbb
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2024-01-05 16:14:10 +0900

    日付テキストの()が全角だったため、半角に変更

--- lib/modal/save_date.dart
+++ lib/modal/save_date.dart
@@ -7,7 +7,7 @@ import '/firestore_reference.dart';
 Future<Null> saveDate(BuildContext context, DateTime focusedDay) async {
   FirestoreReference firestoreReference = FirestoreReference();
   await initializeDateFormatting('ja_JP');
-  String formattedDateTime = DateFormat('yyyy年MM月dd日（EEE）', 'ja_JP').format(focusedDay);
+  String formattedDateTime = DateFormat('yyyy年MM月dd日(EEE)', 'ja_JP').format(focusedDay);
 
   firestoreReference.lastSelectedDay.set({
     'dateString': formattedDateTime,
