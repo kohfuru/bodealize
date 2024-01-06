b0198f543082fa4446a618dcec86312b72a43b8f
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2024-01-06 16:56:10 +0900

    authのcurrentUserがnullの時にエラーが起きないようにnull許容型に変更

--- lib/firestore_reference.dart
+++ lib/firestore_reference.dart
@@ -6,7 +6,7 @@ class FirestoreReference {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
   CollectionReference get users => db.collection('users');
-  String get uid => auth.currentUser!.uid;
+  String? get uid => auth.currentUser?.uid;
   DocumentReference get userDoc => users.doc(uid);
   CollectionReference get selectedDay => userDoc.collection('selectedDay');
   DocumentReference get lastSelectedDay => selectedDay.doc('lastSelectedDay');
