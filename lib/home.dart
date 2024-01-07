52bf45ef63c5c141dd5c299d19d4b6c052dea88a
古谷耕平 <furutanikohei@furutanikoheis-Mac-mini.local>
2024-01-07 16:21:48 +0900

    BottomNavigationBarにcurrentIndex: selectedIndexを追加

--- lib/home.dart
+++ lib/home.dart
@@ -75,6 +75,7 @@ class _HomePageState extends State<HomePage> {
           border: Border(top: BorderSide())
         ),
         child: BottomNavigationBar(
+          currentIndex: selectedIndex,
           backgroundColor: Colors.white,
           items: const [
             BottomNavigationBarItem(
